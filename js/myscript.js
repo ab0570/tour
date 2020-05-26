20582a671ab65ff2c91d759b161f3dc4f5c0bff3
ab0570 <64457532+ab0570@users.noreply.github.com>
2020-05-26 16:10:07 +0900

    5월26일 자바수정

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,42 @@
-var flag =true;
-$(window).resize(function(){
-    var winWidth =$(this).width()
-    if (winWidth > 799 && flag) {
-       $('#header').removeClass('on')
-       flag = false
-    }
+$(window).scroll(function(){
+  var sct= $(this).scrollTop();
+   if ($('html').hasClass('mobile')) {
+      if (sct>50) {
+         $('.to_top').show()
+      } else {
+         $('.to_top').hide()
+      }
+   } else {
+         $('.to_top').show()
+   }
+})
+
+$('.to_top').on('click',function(){
+   $('body,html').animate({scrollTop:'0px'},600)
+})
+
+function init(){
+   var winWidth =$(window).width()
+   if (winWidth > 799) {
+      $('#header').removeClass('on')
+      $('html').addClass('minTb').removeClass('mobile')
+      $('.to_top').show()
+   } else {
+      $('html').addClass('mobile').removeClass('minTb')
+   }
+}
+
+$(window).resize(function(){  
+   init()
+})
+
+$(window).load(function(){
+   init()
 })
 
 $('.depth1 > li > a').on('mouseover focus',function(){
    if ($(this).parents('#header').hasClass('on')) {
-      $(this).next().css({display:'none'})
+      $(this).next().fadeOut(0)
    } else {
       $(this).next().stop().fadeIn(400)
       $(this).parent().siblings().children('.dep2box').stop().fadeOut(400)
@@ -32,3 +59,4 @@ $('#header .openMOgnb').on('click',function(){
 $('#header .closeMOgnb').on('click',function(){
    $(this).parents('#header').removeClass('on')
 })
+
